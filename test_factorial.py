7d1c66a1e16b4643741fa943776107d081c04a80
akullu winnifred <akulluwinnifred1@gmail.com>
2024-07-09 15:10:08 +0300

    the second factorial test

--- test_factorial.py
+++ test_factorial.py
@@ -5,9 +5,11 @@ class TestFactorial(unittest.TestCase):
     def test_factorial_of_zero(self):
         self.assertEqual(factorial(0), 1)
 
-
     def test_factorial_of_one(self):
         self.assertEqual(factorial(1), 1)
+
+    def test_factorial_of_positive_int(self):
+        self.assertEqual(factorial(5), 120)
         
 
 
